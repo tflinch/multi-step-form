47ecab3ed59ee9f59d9d15213395ab655ea1ce54
tflinch <tflinch16@gmail.com>
2025-02-11 15:38:12 -0500

    js setup

---
+++ script.js
@@ -0,0 +1,22 @@
+let currentStep = 1;
+
+function showStep(setp) {
+  //display current setp in the UI
+}
+
+function nextStep() {
+  //handle next step function
+}
+
+function prevStep() {
+  //handles previous step function
+}
+
+document
+  .getElementById('multStepForm')
+  .addEventListener('submit', function (event) {
+    event.preventDefault();
+    alert('Sucess!');
+  });
+
+showStep(currentStep);
